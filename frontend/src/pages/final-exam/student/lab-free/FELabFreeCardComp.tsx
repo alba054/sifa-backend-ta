1c2c31e7ed09fc8492b655e2b82882595e8b1878
YusufSyam <muhyusufsyamsyam@gmail.com>
2022-11-15 01:00:02 +0800

    finish lab free application main page

---
+++ frontend/src/pages/final-exam/student/lab-free/FELabFreeCardComp.tsx
@@ -0,0 +1,100 @@
+import { Button, Group, Stack, Text } from "@mantine/core";
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  FECheckOutline,
+  FECloseOutline,
+  FEDownloadOutline,
+  FEPenOutline,
+  FESearchBookOutline,
+  FETrashOutline,
+  ProgressClockOutlined,
+} from "src/assets/Icons/Fluent";
+
+export interface IFELabFreeCardComp {
+  title: string;
+  lab: string;
+  status: 'process' | 'rejected' | 'accepted';
+  tanggalPermohonan: string;
+}
+
+const FELabFreeCardComp: React.FC<IFELabFreeCardComp> = ({
+  title,
+  lab,
+  status,
+  tanggalPermohonan,
+}) => {
+  const statusChip: any = {
+    'process': (
+      <Group className="bg-[#1E9E63]/[0.15] py-1 px-3 text-[#1E9E63] gap-1 rounded-full max-w-max box-content">
+        <ProgressClockOutlined
+          size={22}
+          color={"#1E9E63"}
+          className="items-center p-[3px]"
+        />
+        <Text className="px-1">Dalam Proses</Text>
+      </Group>
+    ),
+    'rejected': (
+      <Group className="bg-[#FF2C56]/[0.15] py-1 px-3 text-[#FF2C56] gap-1 rounded-full max-w-max box-content">
+        <FECloseOutline
+          color="#FF2C56"
+          size={18}
+          className="items-center p-[3px]"
+        />
+        <Text className="px-1">Ditolak</Text>
+      </Group>
+    ),
+    'accepted': (
+      <Group className="bg-[#5F5AF7]/[0.15] py-1 px-3 text-[#5F5AF7] gap-1 rounded-full max-w-max box-content">
+        <FECheckOutline
+          color="#5F5AF7"
+          size={20}
+          className="items-center p-[3px]"
+        />
+        <Text className="px-1">Diterima</Text>
+      </Group>
+    ),
+  };
+  return (
+    <Group className="flex py-8 px-9 border border-[#DFDFDF] relative justify-between rounded-xl gap-x-10 mx-2">
+      <Stack spacing={"lg"}>
+        {statusChip[`${status}`]}        <Stack className="gap-1">
+          <Text className="text-xs text-secondary-text-200 ">
+            Tanggal Permohonan: {tanggalPermohonan}
+          </Text>
+          <Text className="font-semibold text-[26px] text-primary-text-500 inline-block">
+            {title}
+            {/* <Text className="inline-block text-xs text-secondary-text-200 ">Tanggal Permohonan: 14 November 2022</Text> */}
+          </Text>
+          <Text className="text-secondary-text-500 text-[20px] font-normal">
+            {lab}
+          </Text>
+        </Stack>
+        <Group className="justify-between">
+          <Group spacing={"lg"}>
+            <Link to="#" className={status=="process"? "": "pointer-events-none"}>
+              <FEPenOutline color={status=="process"? "#3B82F6": "#D1D5DB"} />
+            </Link>
+            <Link to="#">
+              <FETrashOutline color="#FF2C56" />
+            </Link>
+          </Group>
+          {status=='accepted'? <Button
+            leftIcon={<FEDownloadOutline size={16} color="#5F5AF7" />}
+            className="bg-primary-500/[0.25] text-primary-500 text-base tracking-wide rounded-lg px-3 pt-[6px] pb-[7px] gap-x-2 absolute right-9 hover:bg-primary-500/[0.25]"
+          >
+            Download Surat
+          </Button> : null}
+          
+        </Group>
+      </Stack>
+      <FESearchBookOutline
+        size={160}
+        color={"#F1F1F3"}
+        className="absolute right-2 z-[-1]"
+      />
+    </Group>
+  );
+};
+export default FELabFreeCardComp;
--- frontend/src/pages/final-exam/student/lab-free/FELabFreeMain.tsx
+++ frontend/src/pages/final-exam/student/lab-free/FELabFreeMain.tsx
@@ -1,41 +1,63 @@
-import { ClockCircleOutlined } from "@ant-design/icons";
-import { Grid, Group, Stack, Text, Title } from "@mantine/core";
+import {
+  Grid,
+  Group,
+  MantineProvider,
+  MediaQuery,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
 import React from "react";
-import { InfoOutline, ProgressClockOutlined } from "src/assets/Icons/Fluent";
+import { InfoOutline } from "src/assets/Icons/Fluent";
+import FELabFreeCardComp from "./FELabFreeCardComp";
 
 export interface IFELabFreeMain {}
 
 const FELabFreeMain: React.FC<IFELabFreeMain> = ({}) => {
   return (
     <Stack mt={24} spacing="xl">
-      <Grid className="bg-[rgb(239,246,255)] rounded-xl p-[24px]" columns={24}>
-        <Grid.Col span={1}>
+      <Group className="bg-[rgb(239,246,255)] rounded-xl p-[28px] pl-[30px] items-start">
+        <MediaQuery smallerThan={"lg"} styles={{ display: "none" }}>
           <InfoOutline size={36} color={"#3B82F6"} />
-        </Grid.Col>
+        </MediaQuery>
 
-        <Grid.Col span={23}>
-          <Stack>
-            <Title className="text-[22px] font-semibold text-[#3B82F6]">
-              Informasi
-            </Title>
-            <Text className="text-[16px] font-semibold text-[#3B82F6]">
-              Setelah mengajukan permohonan, tunggu hingga Kepala Laboratorium
-              yang bersangkutan mengecek permohonan Anda. Jika diterima,
-              silahkan download surat hasil permohonan. Jika ditolak, silahkan
-              lakukan pengajuan ulang jika berkenan.
-            </Text>
-          </Stack>
-        </Grid.Col>
-      </Grid>
-      <Grid>
-        <Grid.Col span={6}>
-          <Group>
-            <Stack>
-              a
-              </Stack>
-          </Group>
-        </Grid.Col>
-      </Grid>
+        <Stack className="w-11/12">
+          <Title className="text-[22px] font-semibold text-[#3B82F6] mt-1">
+            Informasi
+          </Title>
+          <Text className="text-[16px] font-semibold text-[#3B82F6]">
+            Setelah mengajukan permohonan, tunggu hingga Kepala Laboratorium
+            yang bersangkutan mengecek permohonan Anda.{" "}
+            <Text className="font-extrabold inline">Jika diterima</Text>,
+            silahkan download surat hasil permohonan.{" "}
+            <Text className="font-extrabold inline">Jika ditolak</Text>,
+            silahkan lakukan pengajuan ulang jika berkenan.
+          </Text>
+        </Stack>
+      </Group>
+      <MantineProvider
+        withCSSVariables
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          breakpoints: {
+            sm: 800,
+            md: 1000,
+          },
+        }}
+      >
+        <Grid className="mt-2" gutter={"xl"}>
+          <Grid.Col span={6} xs={12} sm={12} md={6}>
+            <FELabFreeCardComp title="Permohonan #3" lab="Lab. Biofarmaka" status="process" tanggalPermohonan="14 November 2022" />
+          </Grid.Col>
+          <Grid.Col span={6} xs={12} sm={12} md={6}>
+            <FELabFreeCardComp title="Permohonan #2" lab="Lab. Farmasetika" status="rejected" tanggalPermohonan="14 November 2022" />
+          </Grid.Col>
+          <Grid.Col span={6} xs={12} sm={12} md={6}>
+            <FELabFreeCardComp title="Permohonan #1" lab="Lab. Farmakologi Toksikologi" status="accepted" tanggalPermohonan="10 November 2022" />
+          </Grid.Col>
+        </Grid>
+      </MantineProvider>
     </Stack>
   );
 };
