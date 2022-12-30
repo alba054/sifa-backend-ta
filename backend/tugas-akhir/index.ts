19a0776750e53cc3840112c603ebb1aa2cb43eaa
alba054 <jralif02@gmail.com>
2022-12-31 00:30:10 +0800

    increase limit of json request body

--- backend/tugas-akhir/index.ts
+++ backend/tugas-akhir/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.disable("x-powered-by");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "11mb" }));
 
 app.use("/api/v0", router);
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerfile));
