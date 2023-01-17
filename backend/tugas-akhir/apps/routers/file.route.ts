ababa20078d433cb61637f61178417f402c564bd
alba054 <jralif02@gmail.com>
2023-01-17 23:22:22 +0800

    able to get sign

--- backend/tugas-akhir/apps/routers/file.route.ts
+++ backend/tugas-akhir/apps/routers/file.route.ts
@@ -19,6 +19,18 @@ fileRouter
       constants.SEMINAR_COORDINATOR_GROUP_ACCESS,
     ]),
     FileHandler.uploadSign
+  )
+  .get(
+    AuthorizationMiddleware.authorize([
+      constants.LECTURER_GROUP_ACCESS,
+      constants.ADMINHEAD_GROUP_ACCCESS,
+      constants.DEAN_GROUP_ACCESS,
+      constants.SUBSECTIONHEAD_GROUP_ACCESS,
+      constants.VOCATION_ADMIN_GROUP_ACCESS,
+      constants.LAB_ADMIN_GROUP_ACCESS,
+      constants.SEMINAR_COORDINATOR_GROUP_ACCESS,
+    ]),
+    FileHandler.getSign
   );
 
 export default fileRouter;
