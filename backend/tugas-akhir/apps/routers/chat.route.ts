382d1c0303d7cb88dbd0a135fe81dd0c823d3b1e
alba054 <jralif02@gmail.com>
2023-01-24 00:00:45 +0800

    chat by nim

--- backend/tugas-akhir/apps/routers/chat.route.ts
+++ backend/tugas-akhir/apps/routers/chat.route.ts
@@ -8,7 +8,7 @@ const chatRouter = express.Router();
 // * get all chats (query : media or text)
 // * send new message
 chatRouter
-  .route("/:thesisID")
+  .route("/:nim")
   .get(
     AuthorizationMiddleware.authorize([
       constants.STUDENT_GROUP_ACCESS,
