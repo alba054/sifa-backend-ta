ac71f9445b683d8a408c43575f13da72c5907c39
alba054 <jralif02@gmail.com>
2023-02-11 06:17:48 +0800

    fix bug

--- backend/tugas-akhir/apps/models/seminar.model.ts
+++ backend/tugas-akhir/apps/models/seminar.model.ts
@@ -648,9 +648,9 @@ export class Seminar {
         };
       });
 
-      await prismaDB.seminar_dokumen.deleteMany({
-        where: { dokSmrId: seminarID },
-      });
+      // await prismaDB.seminar_dokumen.deleteMany({
+      //   where: { dokSmrId: seminarID },
+      // });
       return await prismaDB.seminar_dokumen.createMany({
         data,
       });
